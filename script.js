92e2a8e262452505ad1bced7561d9bba653708aa
emilijajanusevska <97948980+emilijajanusevska@users.noreply.github.com>
2023-04-18 08:41:17 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const plusins = document.querySelector('.js-plus');
-const inputins = document.querySelector('.js-input');
+const multiply = document.querySelector('.js-multiply');
+const minus = document.querySelector('.js-minus');
+const divide = document.querySelector('.js-divide');
+const inputins1 = document.querySelector('.js-input1');
+const inputins2 = document.querySelector('.js-input2');
+const answer = document.querySelector('.js-result');
 
 const Add = (cipars1, cipars2) => {
- return cipars1 + cipars2
+    return cipars1 + cipars2
 }
 
-const Substract = (cipars1, cipars2) => {
-    return cipars1 - cipars2 
+const Subtract = (cipars1, cipars2) => {
+    return cipars1 - cipars2
 }
 
 const Multiply = (cipars1, cipars2) => {
@@ -17,8 +22,31 @@ const Divide = (cipars1, cipars2) => {
     return cipars1 / cipars2
 }
 
-plusins.addEventListener('click', () => {
-   answer = Add(NparseInt(inputins1.value), parseInt (inputins2.value))
+const clear = () => {
+    inputins1.value = ""
+    inputins2.value = ""
+}
 
-   
+plusins.addEventListener('click', () => {
+    answer.textContent = Add(parseInt(inputins1.value),parseInt(inputins2.value))
+
+    clear()
+})
+
+minus.addEventListener('click', () => {
+    answer.textContent = Subtract(parseInt(inputins1.value),parseInt(inputins2.value))
+
+    clear()
+})
+
+multiply.addEventListener('click', () => {
+    answer.textContent = Multiply(parseInt(inputins1.value),parseInt(inputins2.value))
+
+    clear()
+})
+
+divide.addEventListener('click', () => {
+    answer.textContent = Divide(parseInt(inputins1.value),parseInt(inputins2.value))
+
+    clear()
 })
